18dab95dd641cf89e42bd4c7a0d76a5b94f5d153
Kapil Gupta <122624637+CodeFusioner@users.noreply.github.com>
2025-04-01 11:56:25 +0530

    Time: 31 ms (58.53%), Space: 51.5 MB (71.55%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-          Arrays.sort(nums);
+        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
-                int left = i + 1, right = nums.length - 1, target = -nums[i];
-                while (left < right) {
-                    if (nums[left] + nums[right] == target) {
-                        result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                        while (left < right && nums[left] == nums[left + 1]) left++;
-                        while (left < right && nums[right] == nums[right - 1]) right--;
-                        left++;
-                        right--;
-                    } else if (nums[left] + nums[right] < target) {
-                        left++;
-                    } else {
-                        right--;
-                    }
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue; // Skip duplicate elements
+
+            int low = i + 1, high = n - 1;
+            while (low < high) {
+                int sum = nums[i] + nums[low] + nums[high];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                    while (low < high && nums[low] == nums[low + 1])
+                        low++; // Skip duplicate low
+                    while (low < high && nums[high] == nums[high - 1])
+                        high--; // Skip duplicate high
+                    low++;
+                    high--;
+                } else if (sum < 0) {
+                    low++;
+                } else {
+                    high--;
                 }
             }
         }
-
         return result;
     }
 }

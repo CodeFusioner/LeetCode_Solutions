e3af22eb6027006479d1585be6c168a591b62fda
Kapil Gupta <122624637+CodeFusioner@users.noreply.github.com>
2025-04-02 22:21:06 +0530

    Time: 0 ms (100%), Space: 41.2 MB (95.09%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,18 +1,19 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) {
+        // ek ek character kam krte jayenge if indexOf(prefix) != 0
+        // if == 0 then it means it is present in strs[i];
+        if(strs.length == 0 || strs == null){
             return "";
         }
-        String prefix = strs[0];
-        for (int i = 1; i < strs.length; i++) {
-            while (strs[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0, prefix.length() - 1);
-                if (prefix.isEmpty()) {
+        String prefix = strs[0];// let flower as a prefix
+        for(int i = 1;i<strs.length;i++){ // start with the flow
+            while(strs[i].indexOf(prefix) != 0 ){
+                prefix = prefix.substring(0 , prefix.length() -1);
+                if(prefix.isEmpty()){
                     return "";
                 }
             }
         }
-
         return prefix;
     }
 }

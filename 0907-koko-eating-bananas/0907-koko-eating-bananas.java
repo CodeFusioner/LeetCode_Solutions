e7e13df90b09b9bb8006c03a3f70a44da86da49f
Kapil Gupta <122624637+CodeFusioner@users.noreply.github.com>
2025-04-08 10:24:24 +0530

    Time: 6 ms (98.33%), Space: 45.1 MB (60.1%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1;
+        int high = getMaxPile(piles);
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (canEatAll(piles, mid, h)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    public boolean canEatAll(int[] arr, int k, int h) {
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            total += (arr[i] + k - 1) / k;
+        }
+        return total <= h;
+    }
+
+    private int getMaxPile(int[] piles) {
+        int max = 0;
+        for (int pile : piles) {
+            if (pile > max) {
+                max = pile;
+            }
+        }
+        return max;
+    }
+}

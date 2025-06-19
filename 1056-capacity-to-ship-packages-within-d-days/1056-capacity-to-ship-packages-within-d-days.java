684a34136d18fad37d21a3d9215c7a1426ace15b
Kapil Gupta <122624637+CodeFusioner@users.noreply.github.com>
2025-06-19 09:16:26 +0530

    Time: 11 ms (76.41%), Space: 47.2 MB (72.12%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -2,15 +2,14 @@ class Solution {
     public int shipWithinDays(int[] weights, int days) {
         int low = 0;
         int high = 0;
-
-        for(int w : weights){
-            low = Math.max(low, w);
-            high += w;
+        for(int num: weights){
+            low = Math.max(low,num);
+            high += num;
         }
         int ans = high;
-        while(low<=high){
+        while(low <= high){
             int mid = low+(high-low)/2;
-            if(possible(weights,mid,days)){
+            if(helper(weights,mid,days)){
                 ans = mid;
                 high = mid-1;
             }else{
@@ -19,16 +18,16 @@ class Solution {
         }
         return ans;
     }
-    public boolean possible(int[] arr, int mid, int days) {
+    public boolean helper(int [] weights, int mid , int days){
+        int use = 1;
         int load = 0;
-        int usedDays = 1;
-        for(int num : arr){
+        for(int num: weights){
             if(load+num > mid){
                 load = 0;
-                usedDays++;
+                use++;
             }
-            load += num;
+            load+=num;
         }
-        return usedDays <= days;
+        return use <= days;
     }
 }

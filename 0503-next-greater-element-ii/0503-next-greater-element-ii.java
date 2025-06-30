20784d6820f5572142b036d6aae33637e0f9e558
Kapil Gupta <122624637+CodeFusioner@users.noreply.github.com>
2025-06-30 17:12:02 +0530

    Time: 14 ms (76.19%), Space: 46.1 MB (81.88%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -3,13 +3,13 @@ class Solution {
         int n = nums.length;
         int [] arr = new int[n];
         Arrays.fill(arr,-1);
-        for(int i=0;i<n;i++){
-            for(int j = i+1;j<n*2;j++){
-                if(nums[j%n] > nums[i]){
-                    arr[i] = nums[j%n];
-                    break;
-                }
+        Stack<Integer> st = new Stack<>();
+        int j = 0;
+        for(int i=0;i<n*2;i++){
+            while(!st.isEmpty() && nums[i%n] > nums[st.peek()]){
+                arr[st.pop()] = nums[i%n];
             }
+            st.push(i%n);
         }
         return arr;
     }

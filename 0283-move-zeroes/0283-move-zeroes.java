47b7c6573c343c9485a46952da22cfc69d6cd92f
Kapil Gupta <122624637+CodeFusioner@users.noreply.github.com>
2025-07-10 21:32:11 +0530

    Time: 2 ms (80.46%), Space: 46.1 MB (62.35%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,16 +3,14 @@ class Solution {
         int cnt = 0;
         int j = 0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                nums[j] = nums[i];
-                j++;
+            if(nums[i] != 0){
+                nums[j++] = nums[i];
             }else{
                 cnt++;
             }
         }
-        while(cnt > 0){
-            nums[j] = 0;
-            j++;
+        while(cnt>0){
+            nums[j++] = 0;
             cnt--;
         }
     }

81dda47725093188c648990affcf71e80f96e634
Kapil Gupta <122624637+CodeFusioner@users.noreply.github.com>
2025-07-14 15:01:21 +0530

    Time: 30 ms (69.68%), Space: 51 MB (98.54%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,31 +1,29 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
-        int n = nums.length;
-
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue; // Skip duplicate elements
-
-            int low = i + 1, high = n - 1;
-            while (low < high) {
+        for(int i=0;i<nums.length-1;i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int low = i+1, high = nums.length-1;
+            while(low<high){
                 int sum = nums[i] + nums[low] + nums[high];
-                if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[low], nums[high]));
-                    while (low < high && nums[low] == nums[low + 1])
-                        low++; // Skip duplicate low
-                    while (low < high && nums[high] == nums[high - 1])
-                        high--; // Skip duplicate high
+                if(sum == 0){
+                    res.add(Arrays.asList(nums[i],nums[low],nums[high]));
+                    while(low<high && nums[low] == nums[low+1]){
+                        low++;
+                    }
+                    while(low<high && nums[high] == nums[high-1]){
+                        high--;
+                    }
                     low++;
                     high--;
-                } else if (sum < 0) {
+                }else if(sum < 0){
                     low++;
-                } else {
+                }else{
                     high--;
                 }
             }
         }
-        return result;
+        return res;
     }
 }

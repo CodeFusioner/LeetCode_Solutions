bfbd8a58f3196c120c22dcd037dfda4d167109a9
Kapil Gupta <122624637+CodeFusioner@users.noreply.github.com>
2025-07-24 22:20:39 +0530

    Time: 8 ms (16.29%), Space: 45.7 MB (10.91%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,29 +1,30 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-     Stack<Integer> st = new Stack<>();
-        for(int ast : asteroids){
-            boolean exploid = false;
-            while(!st.isEmpty()&&ast<0&&st.peek()>0){
-                if(st.peek()< -ast){
+        Stack<Integer> st = new Stack<>();
+        for(int i=0;i<asteroids.length;i++){
+            if(asteroids[i] > 0){
+                st.push(asteroids[i]);
+            }else{
+                while(!st.isEmpty() && st.peek() > 0 && st.peek() < Math.abs(asteroids[i])){
                     st.pop();
-                } else if (st.peek() == -ast) {
+                } 
+                if(!st.isEmpty() && st.peek() == Math.abs(asteroids[i])){
                     st.pop();
-                    exploid = true;
-                    break;
-                }else{
-                    exploid = true;
-                    break;
+                }else if(st.isEmpty() || st.peek() < 0){
+                    st.push(asteroids[i]);
                 }
             }
-            if(!exploid){
-                st.push(ast);
-            }
         }
-        int [] ans = new int[st.size()];
-        for(int i = 0;i<st.size();i++){
-            ans[i] = st.get(i);
+        int [] arr = new int[st.size()];
+        int j = 0;
+        Stack<Integer> st2 = new Stack<>();
+        while(!st.isEmpty()){
+            st2.push(st.pop());
         }
-        return ans;
+        while(!st2.isEmpty()){
+            arr[j++] = st2.pop();
+        }
 
+        return arr;
     }
 }

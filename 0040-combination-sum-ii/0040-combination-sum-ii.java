b3e02b4222c1fbb1d84707164aac01429ee8cb6e
Kapil Gupta <122624637+CodeFusioner@users.noreply.github.com>
2025-07-30 14:55:59 +0530

    Time: 3 ms (72.49%), Space: 43.1 MB (87.03%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -7,13 +7,13 @@ class Solution {
     }
     public static void sum(List<List<Integer>> ll,int target,List<Integer> ll1,int idx,int[] arr ){
         if(target==0){ 
-            if(!ll.contains(ll1))
+            // if(!ll.contains(ll1))
             ll.add(new ArrayList<>(ll1));
             return;
         }
         if(target<0||idx>=arr.length) return;
           for(int i=idx;i<arr.length;i++){
-            if(i>0&&i>idx&&arr[i]==arr[i-1]) continue;
+             if(i>0&&i>idx&&arr[i]==arr[i-1]) continue;
             if(target-arr[i]>=0) {
                 ll1.add(arr[i]);
                 sum(ll,target-arr[i],ll1,i+1,arr);

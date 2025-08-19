0389ab2eac897b75480bf4606eedd6567d5a5530
Kapil Gupta <122624637+CodeFusioner@users.noreply.github.com>
2025-08-19 17:29:37 +0530

    Time: 9 ms (44.26%), Space: 46.4 MB (86.2%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,24 +1,20 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        return mergeIntervals(intervals);
-    }
-    public int [][] mergeIntervals(int [][] arr){
-        if(arr.length == 0){
+        if(intervals.length == 0){
             return new int[0][0];
         }
-        Arrays.sort(arr , (a,b) -> Integer.compare(a[0],b[0]));
-        LinkedList<int[]> res = new LinkedList<>();
-        res.add(arr[0]);
-        for(int i = 1;i<arr.length;i++){
-            int [] lastArr = res.getLast();
-            if(lastArr[1] >= arr[i][0]){
-                lastArr[1] = Math.max(lastArr[1] , arr[i][1]);
+       LinkedList<int[]> list = new LinkedList<>();
+       
+    Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+    list.add(intervals[0]);
+       for(int i=1;i<intervals.length;i++){
+            int [] prev = list.getLast();
+            if(prev[1] >= intervals[i][0]){
+                prev[1] = Math.max(prev[1],intervals[i][1]);
             }else{
-                res.add(arr[i]);
+                list.add(intervals[i]);
             }
-        }
-
-        return res.toArray(new int[res.size()][]);
-
+       }
+       return list.toArray(new int[list.size()][]);
     }
 }
